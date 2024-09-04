7fb1804bdfec5d5b40297b918fadf2bc3b06e3c4
Anatoly Trosinenko <atrosinenko@accesssoftek.com>
2024-09-04 21:45:45 +0300

    Build aarch64-linux-musl sysroot alongside aarch64-linux-pauthtest

--- build-all.sh
+++ build-all.sh
@@ -4,10 +4,20 @@ cd "$(dirname "$0")"
 . ./config
 
 ./build-llvm.sh
-./build-wrapper.sh
-./build-linux-header.sh
-LIBC_STARTFILE_STAGE=1 ./build-musl.sh
-./build-compiler-rt.sh
-./build-musl.sh
-./build-runtimes.sh
-COMPILER_RT_FULL_BUILD=1 ./build-compiler-rt.sh
+
+build_target_libs() {
+  ./build-wrapper.sh
+  ./build-linux-header.sh
+  LIBC_STARTFILE_STAGE=1 ./build-musl.sh
+  ./build-compiler-rt.sh
+  ./build-musl.sh
+  ./build-runtimes.sh
+  COMPILER_RT_FULL_BUILD=1 ./build-compiler-rt.sh
+}
+
+build_target_libs
+
+export CROSS_TARGET="aarch64-linux-musl"
+export TARGET_PREFIX="$OUTPUT_DIR/$CROSS_TARGET/usr"
+
+build_target_libs
